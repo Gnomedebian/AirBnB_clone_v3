7edb8c67a98eea476f4eac7b7e44b2d373280785
Gnomedebian <yassineamgarou@gmail.com>
2024-03-31 14:29:39 +0000

    task::v3

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,12 +3,10 @@
 from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
-from flasgger.utils import swag_from
 from flask import abort, jsonify, make_response, request
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/amenity/all_amenities.yml')
 def get_amenities():
     """Retrieves a list of all amenities"""
     all_amenities = storage.all(Amenity).values()
@@ -20,7 +18,6 @@ def get_amenities():
 
 @app_views.route('/amenities/<amenity_id>/', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/get_amenity.yml', methods=['GET'])
 def get_amenity(amenity_id):
     """ Retrieves an amenity """
     amenity = storage.get(Amenity, amenity_id)
@@ -32,7 +29,6 @@ def get_amenity(amenity_id):
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/delete_amenity.yml', methods=['DELETE'])
 def delete_amenity(amenity_id):
     """Deletes an amenity  Object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -47,7 +43,6 @@ def delete_amenity(amenity_id):
 
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/amenity/post_amenity.yml', methods=['POST'])
 def post_amenity():
     """Creates an amenity"""
     if not request.get_json():
@@ -64,7 +59,6 @@ def post_amenity():
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
-@swag_from('documentation/amenity/put_amenity.yml', methods=['PUT'])
 def put_amenity(amenity_id):
     """Updates an amenity"""
     if not request.get_json():
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,12 +3,10 @@
 from models.user import User
 from models import storage
 from api.v1.views import app_views
-from flasgger.utils import swag_from
 from flask import abort, jsonify, make_response, request
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/user/all_users.yml')
 def get_users():
     """Retrieves the list of all user objects"""
     all_users = storage.all(User).values()
@@ -19,7 +17,6 @@ def get_users():
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/user/get_user.yml', methods=['GET'])
 def get_user(user_id):
     """Retrieves an user"""
     user = storage.get(User, user_id)
@@ -31,7 +28,6 @@ def get_user(user_id):
 
 @app_views.route('/users/<user_id>', methods=['DELETE'],
                  strict_slashes=False)
-@swag_from('documentation/user/delete_user.yml', methods=['DELETE'])
 def delete_user(user_id):
     """Deletes a user Object"""
     user = storage.get(User, user_id)
@@ -46,7 +42,6 @@ def delete_user(user_id):
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/user/post_user.yml', methods=['POST'])
 def post_user():
     """Creates a user"""
     if not request.get_json():
@@ -64,7 +59,6 @@ def post_user():
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/user/put_user.yml', methods=['PUT'])
 def put_user(user_id):
     """Updates a user"""
     user = storage.get(User, user_id)
